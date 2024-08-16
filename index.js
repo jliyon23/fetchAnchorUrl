40803af390627898aff474f16b8e2093b723a351
Joseph Liyon PT <josephliyon23@gmail.com>
2024-08-16 21:42:46 +0530

    Removed token generation

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
-const axios = require("axios");
-const querystring = require("querystring");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -42,45 +40,9 @@ app.get("/fetch-recaptcha", async (req, res) => {
 
     await browser.close();
 
-    if (recaptchaURL) {
-      const anchorr = recaptchaURL;
-      const keysite = anchorr.split("k=")[1].split("&")[0];
-      const var_co = anchorr.split("co=")[1].split("&")[0];
-      const var_v = anchorr.split("v=")[1].split("&")[0];
-
-      try {
-        const response = await axios.get(anchorr);
-        const r1 = response.data;
-        const token1 = r1.split('recaptcha-token" value="')[1].split('">')[0];
-
-        const payload = {
-          v: var_v,
-          reason: "q",
-          c: token1,
-          k: keysite,
-          co: var_co,
-          hl: "en",
-          size: "invisible",
-        };
-
-        const bypassResponse = await axios.post(
-          `https://www.google.com/recaptcha/api2/reload?k=${keysite}`,
-          querystring.stringify(payload)
-        );
-        const r2 = bypassResponse.data;
-        const token2 = r2.split('"rresp","')[1]?.split('"')[0] || "null";
-
-        res.json({
-          anchorUrl: recaptchaURL,
-          xToken: token2,
-        });
-      } catch (error) {
-        console.error("Error during reCAPTCHA bypass:", error);
-        res.status(500).send("An error occurred during reCAPTCHA bypass");
-      }
-    } else {
-      res.status(404).send("reCAPTCHA URL not found");
-    }
+    res.json({
+        anchorUrl: recaptchaURL,
+    })
   } catch (error) {
     console.error("Error during reCAPTCHA URL fetching:", error);
     res.status(500).send("An error occurred while fetching reCAPTCHA URL");
